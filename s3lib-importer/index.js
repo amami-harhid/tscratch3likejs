d718170c104449c83da9ad908984efb93862eae6
amami-harhid <amami-thida100@outlook.jp>
2025-03-17 12:28:00 +0900

    update

--- s3lib-importer/index.js
+++ s3lib-importer/index.js
@@ -1,7 +1,9 @@
 const githubPage = "https://amami-harhid.github.io/scratch3likejslib";
 const jsName = "build/likeScratchLib.js";
-const {PlayGround, Library} = await import(`${githubPage}/${jsName}`);
+//const {PlayGround, Library} = await import(`${githubPage}/${jsName}`);
 //from 'scratch3likejslib/build/likeScratchLib.js';
+import {PlayGround, Library} from '@scratch3likejslib/build';
+
 const Pg = PlayGround;
 const Lib = Library;
 
