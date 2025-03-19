cf18340d8b54e54088010ebf7b549f264d882247
amami-harhid <amami-thida100@outlook.jp>
2025-03-19 10:54:38 +0900

    update

--- types/scratchjs/s3Stage/index.d.ts
+++ types/scratchjs/s3Stage/index.d.ts
@@ -1,7 +1,7 @@
-import {S3ImageFunctions} from "../s3ImageFunctions";
-import {S3SoundFunctions} from "../s3SoundFunctions";
-import {S3ControlFunctions} from "../s3ControlFunctions";
 import {S3EventFunctions} from "../s3EventFunctions";
+import {S3ImageFunctions} from "../s3ImageFunctions";
+import {S3LooksFunctions} from "../s3LooksFunctions";
+import {S3SoundFunctions} from "../s3SoundFunctions";
 import {S3Entity} from '../s3Entity';
 
 /** イベント処理 */
@@ -11,6 +11,12 @@ declare interface S3StageEventFunctions extends S3EventFunctions{
     /** 背景を切り替える */
     switchBackdrop(val: string | number ) : void;
 }
+declare interface S3StageLooksFunctions extends S3LooksFunctions{
+    /** 次の背景にする */
+    nextBackdrop(): void;
+    /** 指定した名前(または番号)で背景を切り替える */
+    switchBackdrop(backdrop: string | number): void;
+ }
 
 /** ステージ（実体[Entity]を継承）*/
 export interface S3Stage extends S3Entity{
@@ -19,5 +25,5 @@ export interface S3Stage extends S3Entity{
     Event: S3StageEventFunctions;
     Control: S3ControlFunctions;
     Sound: S3SoundFunctions;
-    Looks: S3StageEventFunctions;
+    Looks: S3StageLooksFunctions;
 }
