3266f4205a7099fe579c985d0e5f1ce31671d645
amami-harhid <amami-thida100@outlook.jp>
2025-03-26 10:31:14 +0900

    update

--- types/scratchjs/s3SoundFunctions/index.d.ts
+++ types/scratchjs/s3SoundFunctions/index.d.ts
@@ -6,13 +6,43 @@ declare interface S3SoundProperties {
 /** サウンドメソッド */
 export interface S3SoundFunctions {
     /** サウンド追加 */
-    add(sound: any) : Promise<any>;
-    switch(name: string): void;
+    /**
+     * ロード済のサウンドを追加する
+     * @param soundName サウンド名
+     */
+    add(soundName: string) : Promise<any>;
+    /**
+     * サウンドを切り替える
+     * @param soundName サウンド名
+     */
+    switch(soundName: string): void;
+    /**
+     * 次のサウンドに切り替える
+     */
     next(): void;
-    play(): void;
-    playUntilDone(): Promise<any>;
+    /**
+     * サウンドを鳴らす
+     * @param soundName サウンド名
+     */
+    play(soundName: string): void;
+    /**
+     * 終わるまでサウンドを鳴らす
+     * @param soundName サウンド名
+     */
+    playUntilDone(soundName: string): Promise<any>;
     /** サウンドオプション設定 */
+    /**
+     * サウンドのオプションを設定する
+     * @param key オプションキー
+     * @param value オプション量
+     */
     setOption(key: string, value: number): Promise<any>;
+    /**
+     * サウンドを停止する
+     */
     stop(): void;
+    /**
+     * サウンドを即時に停止する
+     */
     stopImmediately(): void;
 }
