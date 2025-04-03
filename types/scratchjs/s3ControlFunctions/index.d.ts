cf42fab768902558785d26079b68aed4cf8359d9
amami-harhid <amami-thida100@outlook.jp>
2025-04-03 15:02:12 +0900

    2025/4/3 15:02

--- types/scratchjs/s3ControlFunctions/index.d.ts
+++ types/scratchjs/s3ControlFunctions/index.d.ts
@@ -14,4 +14,6 @@ export interface S3ControlFunctions {
     repeat(count: number, process:Function ) : Promise<any>;
     /** 指定した条件が成立するまで、繰り返す(await任意) */
     repeatUntil(condition: Function, process:Function ) : Promise<any>;
+    /** 全てを停止する */
+    stopAll() : void;
 }
