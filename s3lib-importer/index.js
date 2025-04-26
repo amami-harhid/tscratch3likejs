19de08f231bb3f55be2a6749a2ece3c50e00e83d
amami-harhid <amami-thida100@outlook.jp>
2025-04-26 15:18:13 +0900

    update

--- s3lib-importer/index.js
+++ s3lib-importer/index.js
@@ -1,7 +1,7 @@
 /**
  * import script
  */
-import {PlayGround, Library} from '@amami-harhid/scratch3likejslib/build';
+import {PlayGround, Library} from '@amami-harhid/scratch3likejslib/build/index.js';
 
 const Pg = PlayGround;
 const Lib = Library;
